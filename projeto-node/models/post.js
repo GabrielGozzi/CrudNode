3e9edaec7e43371ced8c718ea833280291af4721
GabrielGozzi <biel20032010@gmail.com>
2023-09-05 20:33:53 -0300

    Crud Basico

---
+++ projeto-node/models/post.js
@@ -0,0 +1,29 @@
+const db = require("./banco")
+
+const Agendamentos = db.sequelize.define('agendamentos',{
+    nome:{
+        type: db.Sequelize.STRING
+    },
+    telefone:{
+        type: db.Sequelize.INTEGER
+    },
+    origem:{
+        type: db.Sequelize.STRING
+    },
+    data_contato:{
+        type: db.Sequelize.DATE
+    },
+    endereco:{
+        type: db.Sequelize.STRING
+    },
+    complemento:{
+        type: db.Sequelize.STRING
+    },
+    observacao:{
+        type: db.Sequelize.TEXT
+    }
+})
+
+//Agendamentos.sync({force:true})
+
+module.exports = Agendamentos
